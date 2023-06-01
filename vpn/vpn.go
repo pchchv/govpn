9fd09823bc9d7efbff1bda19554e807a218c7ac6
pchchv <ipchchv@gmail.com>
2023-06-01 11:28:45 +0300

    Implement a CreateVpn func

---
+++ vpn/vpn.go
@@ -0,0 +1,22 @@
+package vpn
+
+import (
+	"log"
+
+	"github.com/pchchv/govpn/common/osutil"
+	"github.com/songgao/water"
+)
+
+func CreateVpn(cidr string) (iface *water.Interface) {
+	c := water.Config{DeviceType: water.TAP}
+	iface, err := water.New(c)
+	if err != nil {
+		log.Fatalln("failed to allocate vpn interface:", err)
+	}
+
+	log.Println("interface allocated:", iface.Name())
+
+	osutil.ConfigVpn(cidr, iface)
+
+	return iface
+}
