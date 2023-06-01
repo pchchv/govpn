e9738b7af99d96910f10472ca43245c98753baab
pchchv <ipchchv@gmail.com>
2023-06-01 11:41:47 +0300

    Implement a XOR

--- common/cipher/cipher.go
+++ common/cipher/cipher.go
@@ -1,7 +1,24 @@
 package cipher
 
+import (
+	"crypto/rc4"
+	"log"
+)
+
 var _key []byte
 
 func GenerateKey(key string) {
 	_key = []byte(key)
+}
+
+func XOR(src []byte) []byte {
+	c, err := rc4.NewCipher(_key)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	dst := make([]byte, len(src))
+	c.XORKeyStream(dst, src)
+
+	return dst
 }
