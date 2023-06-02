eb44f27c79eaa8d444f231078812514d3bbbc5c3
pchchv <ipchchv@gmail.com>
2023-06-02 13:05:02 +0300

    Implement an incr func

--- register/register.go
+++ register/register.go
@@ -14,3 +14,19 @@ func checkIPv4(ip net.IP) net.IP {
 
 	return ip
 }
+
+func incr(IP net.IP) net.IP {
+	IP = checkIPv4(IP)
+	incIP := make([]byte, len(IP))
+
+	copy(incIP, IP)
+
+	for j := len(incIP) - 1; j >= 0; j-- {
+		incIP[j]++
+		if incIP[j] > 0 {
+			break
+		}
+	}
+
+	return incIP
+}
